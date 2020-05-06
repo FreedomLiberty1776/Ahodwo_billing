b4e0c6ff9fba6d6ff679d1eb164049836ba85147
Emmanuel Adu <aduemmanuelk21@gmail.com>
2020-05-06 15:12:41 -0400

    initial commit

---
+++ pages/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+urlpatterns = [
+	path('', views.index, name='index'),
+	path('createinvoice', views.createinvoice, name='createinvoice'),
+	path('properties', views.properties, name='properties'),
+	path('agent', views.agent, name='agent'),
+	path('service', views.service, name='service'),
+	path('property_type', views.property_type, name='property_type'),
+	path('payment_method', views.payment_method, name='payment_method'),
+	
+
+	# path('invoice/', views.ViewPDF.as_view(), name="invoice"),
+  # path('pdf_download/', views.DownloadPDF.as_view(), name="pdf_download"),
+]
